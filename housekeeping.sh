c4805ccecde1fe0390824c959f11bfb5fb721371
mightyBroccoli <nico@magicbroccoli.de>
2017-10-02 16:14:05 +0200

    * optimised some parts + added configtest enabled users to see what would have happend

--- housekeeping.sh
+++ housekeeping.sh
@@ -7,7 +7,7 @@
 ###### CONFIGURATION ######
 # configuration variables
 tmp_directory=/tmp/prosody/
-accounts_to_delete=$tmp_directory/accounts_to_delete.txt
+junk_to_delete=$tmp_directory/accounts_to_delete.txt
 
 # maximum timeframe for accounts registered but not logged in
 # needs to be in the syntax 1day 2weeks 3months 4years
@@ -45,45 +45,65 @@ clearcomp()
 {
 	if [ "$1" = "-all" ]; then
 		# run the created script to delete the selected accounts
-		bash $accounts_to_delete
+		bash $junk_to_delete
 
 		#remove all tmp files and variables for privacy reasons
-		rm -f "$accounts_to_delete"
-		unset unused_accounts_timeframe old_accounts_timeframe mam_message_live enable_mam_clearing prosody_db_user prosody_db_password accounts_to_delete
+		rm -f "$junk_to_delete"
+		unset unused_accounts_timeframe old_accounts_timeframe mam_message_live enable_mam_clearing prosody_db_user prosody_db_password junk_to_delete
 	fi
 
 	# remove the temp files
-	rm -f "$accounts_to_delete"
+	rm -f "$junk_to_delete"
 }
 
 catch_help()
 {
 	# catch  -h / --help
-	if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+	if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 		display_help
+		exit
 	fi
-	exit
+}
+
+catch_configtest()
+{
+	# test your configuration first to see what would have be deleted
+	if [ "$1" == "-t" ] || [ "$1" == "--configtest" ]; then
+		filter_unused_accounts
+		filter_old_accounts
+		filter_mam_messages --test
+
+		cat $junk_to_delete
+		exit
+	fi
 }
 
 ###### FILTER SECTION ######
 filter_unused_accounts()
 {
 	# filter all registered but not logged in accounts older then $unused_accounts_timeframe
-	prosodyctl mod_list_inactive magicbroccoli.de "$unused_accounts_timeframe" event | grep registered | sed 's/registered//g' | sed -e 's/^/prosodyctl deluser /' >> $accounts_to_delete
+	prosodyctl mod_list_inactive magicbroccoli.de "$unused_accounts_timeframe" event | grep registered | sed 's/registered//g' | sed -e 's/^/prosodyctl deluser /' >> $junk_to_delete
 }
 
 filter_old_accounts()
 {
 	# filter all inactive accounts older then $old_accounts_timeframe
-	prosodyctl mod_list_inactive magicbroccoli.de "$old_accounts_timeframe" | sed -e 's/^/prosodyctl deluser /' >> $accounts_to_delete
+	prosodyctl mod_list_inactive magicbroccoli.de "$old_accounts_timeframe" | sed -e 's/^/prosodyctl deluser /' >> $junk_to_delete
 }
 
 filter_mam_messages()
 {
 	# only run this filter if $enable_mam_clearing is set to true
-	if [ "$enable_mam_clearing" = true ]; then
+	if [ "$enable_mam_clearing" = "true" ]; then
+		# catch config test
+		if [ "$1" = "--test" ]; then
+			# this is currently a workaround caused by the extrem slowness of prosodys own clearing mechanism
+			# filter all expired mod_mam messages from archive
+			echo "SELECT * FROM prosody.prosodyarchive WHERE \`when\` < UNIX_TIMESTAMP(DATE_SUB(curdate(),INTERVAL $mam_message_live));" | mysql -u $prosody_db_user -p$prosody_db_password &>> $junk_to_delete
+			return 1
+		fi
 		# this is currently a workaround caused by the extrem slowness of prosodys own clearing mechanism
-		# filter all expired mod_mam messages from archive
+		# delete all expired mod_mam messages from archive
 		echo "DELETE FROM prosody.prosodyarchive WHERE \`when\` < UNIX_TIMESTAMP(DATE_SUB(curdate(),INTERVAL $mam_message_live));" | mysql -u $prosody_db_user -p$prosody_db_password
 	fi
 }
@@ -91,7 +111,7 @@ filter_mam_messages()
 filter_expired_http_uploads()
 {
 	# currently a workaround as the mod_http_uploud is not removing the folder which holds the file
-	find $http_upload_path/* -maxdepth 0 -type d -mtime +$http_upload_expire | xargs rm -rf
+	find $http_upload_path/* -maxdepth 0 -type d -mtime +$http_upload_expire | sed -e 's/^/rm -rf /' >> $junk_to_delete
 }
 
 ###### General Functions ######
@@ -106,9 +126,11 @@ display_help()
 
 ###### MAIN CODE SECTION ######
 # catch user help input
-catch_help
+catch_help "$@"
 # check env and some tests
 prerun_check
+# catch user config test
+catch_configtest "$@"
 
 # unused accounts filter
 filter_unused_accounts
