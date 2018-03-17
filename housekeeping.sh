ca5d891d415ab4b2288e2787774213dd2c6b3a13
nico <nico.wellpott@uni-oldenburg.de>
2018-03-17 01:26:58 +0100

    merge dev into master (#10)
    + read mysql credentials from prosody.cfg.lua ( ref.: #9 )
    + added alpha multi host support ( ref.: #6 )
    - removed unnecessary cat love
    - removed unnecessary echo
    * some shellcheck fixes
    + introduced prosody_config_location as variable for the global config
    * removed possible malfunction in retrieving the username/password from config
    * updated exit code definitions
    + added skriptconfig version check
    + added config test safety catch
    * suppress the output of the filename after using wc
    + added ignore_file backup
    * correct exception on \\n with printf
    + implemented account removal with telnet interface
    - removed prepared_list and bash execution
    * fixed some bugs

--- housekeeping.sh
+++ housekeeping.sh
@@ -16,15 +16,17 @@ unused_accounts=$tmp_directory/unused_accounts.txt
 old_accounts=$tmp_directory/old_accounts.txt
 junk_to_delete=$tmp_directory/junk_to_delete.txt
 dbjunk_to_delete=$tmp_directory/dbjunk_to_delete.txt
-prepared_list=$tmp_directory/prepared_list.txt
+prepared_accounts=$tmp_directory/prepared_accounts.txt
 
 # external config file
+script_version="1.0.1"
 configfile=$tmp_directory/.user.config
 configfile_secured=$tmp_directory/tmp.config
 backupconf=/var/backups/prosody_housekeeping.user.config
 
 # external ignore file
 ignored_accounts=$tmp_directory/ignored_accounts.txt
+ignore_backup=/var/backups/prosody_housekeeping.ignored_accounts.backup
 
 ###### PRE RUN FUNCTION SECTION ######
 prerun_check()
@@ -41,10 +43,12 @@ prerun_check()
 
 	if ((missing_counter > 0)); then
 		log_to_file "$(printf "Minimum %d commands are missing in PATH, aborting\\n" "$missing_counter" >&2)"
+
+		# exit code for missing commands in PATH
 		exit 11
 	fi
 
-	#check if everything is present if not create it
+	# check if everything is present if not create it
 	if [ ! -d "$tmp_directory" ]; then
 		mkdir -p "$tmp_directory"
 	fi
@@ -57,11 +61,13 @@ prerun_check()
 	# check for presents of the configfile if not exit
 	if [ ! -f "$configfile" ]; then
 		if [ -f "$backupconf" ]; then
-			log_to_file "$(echo "no config inside $tmp_directory using $backupconf")"
+			log_to_file "no config inside $tmp_directory using $backupconf"
 			cp "$backupconf" "$configfile"
 		else
 			#config file is not present
-			log_to_file "$(echo "no config file has been set. copy the sample config file to $configfile")"
+			log_to_file "no config file has been set. copy the sample config file to $configfile"
+
+			# exit code for missing config file
 			exit 10
 		fi
 	else
@@ -76,7 +82,32 @@ prerun_check()
 	fi
 
 	# source the config file
+	# shellcheck source=$tmp_directory/.user.config
+	# shellcheck disable=SC1091
 	source  $configfile
+
+	# check if the latest config version is used to prevent bad stuff from happening
+	if [ ! "$script_version" == "$conf_version" ]; then
+		# throw error on outdated config file
+		log_to_file "Error: Your config file is outdated. Please update your config file to proceed."
+
+		# exit code for outdated config file
+		exit 2
+	fi
+
+	# checking if ignore file is present
+	if [ ! -f "$ignored_accounts" ]; then
+		if [ -f "ignore_backup" ]; then
+			log_to_file "ignore file missing, using backup $ignore_backup"
+			cp "$ignore_backup" "ignored_accounts"
+		else
+			log_to_file "no ignore file present,creating one ..."
+			touch "$ignored_accounts"
+		fi
+	else
+		# copy ignore file to /var/backups
+		cp "$ignored_accounts" "$ignore_backup"
+	fi
 
 	# clear env
 	clearcomp
@@ -87,7 +118,9 @@ catch_help()
 	# catch  -h / --help
 	if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 		display_help
-		exit
+
+		# gracefull exit
+		exit 0
 	fi
 }
 
@@ -102,21 +135,23 @@ catch_configtest()
 
 		# Only present files if they are present
 		if [ -s $unused_accounts ]; then
-			echo -e "Registration expired: \\n$(cat $unused_accounts)\\n"
+			printf "Registration expired:\\n%s\\n" "$(<$unused_accounts)"
 		fi
 
 		if [ -s $old_accounts ]; then
-			echo -e "unused Accounts: \\n$(cat $old_accounts)\\n"
+			printf "unused Accounts:\\n%s\\n" "$(<$old_accounts)"
 		fi
 
 		if [ -s $junk_to_delete ]; then
-			echo -e "expired HTTP_Upload Folders: \\n$(cat $junk_to_delete)\\n"
+			printf "expired HTTP_Upload Folders:\\n%s\\n" "$(<$junk_to_delete)"
 		fi
 
 		if [ -s $dbjunk_to_delete ]; then
-			echo -e "MAM Entries marked for deletion: \\n$(wc -l $dbjunk_to_delete)\\n"
+			printf "MAM Entries marked for deletion:\\n%i\\n" "$(< $dbjunk_to_delete wc -l)"
 		fi
-		exit
+
+		# gracefully exit the config test
+		exit 0
 	fi
 }
 
@@ -132,32 +167,36 @@ display_help()
 ###### FILTER SECTION ######
 filter_unused_accounts()
 {
-	# only run this filter if its enabled
-	if [ "$enable_unused" = "true" ]; then
-		# filter all registered but not logged in accounts older then $unused_accounts_timeframe
-		prosodyctl mod_list_inactive "$host" "$unused_accounts_timeframe" event | grep registered | sed 's/registered//g' > "$composition"
+	for tld in "${host[@]}"; do
+		# only run this filter if its enabled
+		if [ "$enable_unused" = "true" ]; then
+			# filter all registered but not logged in accounts older then $unused_accounts_timeframe
+			prosodyctl mod_list_inactive "$tld" "$unused_accounts_timeframe" event | grep registered | sed 's/registered//g' > "$composition"
 
-		# if there are any accounts selected
-		if [ -s "$composition" ]; then
-			# filter out ignored accounts
-			filter_ignored_accounts > "$unused_accounts"
+			# if there are any accounts selected
+			if [ -s "$composition" ]; then
+				# filter out ignored accounts
+				filter_ignored_accounts > "$unused_accounts"
+			fi
 		fi
-	fi
+	done
 }
 
 filter_old_accounts()
 {
-	# only run this filter if its enabled
-	if [ "$enable_old" = "true" ]; then
-		# filter all accounts logged out $old_accounts_timeframe in the past
-		prosodyctl mod_list_inactive "$host" "$old_accounts_timeframe" event | grep logout | sed 's/logout//g' | sed 's/ //g' > "$composition"
+	for tld in "${host[@]}"; do
+		# only run this filter if its enabled
+		if [ "$enable_old" = "true" ]; then
+			# filter all accounts logged out $old_accounts_timeframe in the past
+			prosodyctl mod_list_inactive "$host" "$old_accounts_timeframe" event | grep logout | sed 's/logout//g' | sed 's/ //g' > "$composition"
 
-		# if there are any accounts selected
-		if [ -s "$composition" ]; then
-			# filter out ignored accounts
-			filter_ignored_accounts > "$old_accounts"
+			# if there are any accounts selected
+			if [ -s "$composition" ]; then
+				# filter out ignored accounts
+				filter_ignored_accounts > "$old_accounts"
+			fi
 		fi
-	fi
+	done
 }
 
 filter_ignored_accounts()
@@ -201,18 +240,72 @@ filter_mam_messages()
 
 		# only log this if garbage collection actually deleted stuff
 		if [ -s $dbjunk_to_delete ]; then
-			log_to_file "MAM garbage collection removed $(wc -l $dbjunk_to_delete) lines from the database."
+			log_to_file "MAM garbage collection removed $(wc -l < $dbjunk_to_delete) lines from the database."
 		fi
 	fi
 }
 
 ###### General Functions ######
+log_to_file()
+{
+	# ghetto logging
+	echo "[$(date --rfc-3339=seconds)] - $*" >> "$logfile"
+}
+
+prepare_execution()
+{
+	if [ -s "$unused_accounts" ]; then
+		rm -f "$composition"
+		# prepare selected user list to be removed
+		while read -r line; do
+			if [ "$logging" = "true" ]; then
+				# read the files line by line and prepend and append some info
+				log_to_file "$(echo -e "$line" | sed -e 's/^/Registration expired: /')"
+			fi
+				echo "user:delete([[$line]])" >> "$composition"
+		done < "$unused_accounts"
+
+		# concatenate all accounts together for removal
+		cat "$composition" >> "$prepared_accounts"
+	fi
+
+	if [ -s "$old_accounts" ]; then
+		rm -f "$composition"
+		# prepare selected user list to be removed
+		while read -r line; do
+			if [ "$logging" = "true" ]; then
+				# read the files line by line and prepend and append some info
+				log_to_file "$(echo -e "$line" | sed -e 's/^/Account expired: /')"
+			fi
+				echo "user:delete([[$line]])" >> "$composition"
+		done < "$old_accounts"
+
+		# concatenate all accounts together for removal
+		cat "$composition" >> "$prepared_accounts"
+	fi
+}
+
 clearcomp()
 {
 	if [ "$1" = "-removal" ]; then
+		# hand the deletion to the prosody telnet interface
+		if [ -s $prepared_accounts ]; then
+			(
+			while read -r line; do
+				echo "$line"
+			done < "$prepared_accounts"
+			echo quit
+			) | nc localhost 5582 &>/dev/null
+		fi
+
 		# run the created script to delete the selected accounts
-		if [ -s $prepared_list ]; then
-			bash "$prepared_list"
+		if [ -s $junk_to_delete ]; then
+			while read -r line; do
+				if [ "$logging" = "true" ]; then
+					log_to_file "$(echo -e "$line" | sed -e 's/^/Folder: "/' | sed 's/$/" has been marked for removal./')"
+				fi
+				rm -rf "$line"
+			done < "$junk_to_delete"
 		fi
 
 		# ISSUE #5
@@ -222,63 +315,18 @@ clearcomp()
 		fi
 
 		# removal of tmp files
-		rm -f "$composition" "$unused_accounts" "$old_accounts" "$junk_to_delete" "$dbjunk_to_delete" "$prepared_list"
+		rm -f "$composition" "$unused_accounts" "$old_accounts" "$junk_to_delete" "$dbjunk_to_delete" "$prepared_accounts"
 
 		# remove variables for privacy reasons
-		unset tmp_directory logfile composition unused_accounts old_accounts junk_to_delete dbjunk_to_delete prepared_list logging host enable_unused unused_accounts_timeframe enable_old
+		unset tmp_directory logfile composition unused_accounts old_accounts junk_to_delete dbjunk_to_delete prepared_accounts logging host enable_unused unused_accounts_timeframe enable_old
 		unset old_accounts_timeframe enable_mam_clearing mam_message_live prosody_db_user prosody_db_password enable_http_upload http_upload_path http_upload_expire
 		exit 0
 	fi
 
 	# removal of tmp files
-	rm -f "$composition" "$unused_accounts" "$old_accounts" "$junk_to_delete" "$dbjunk_to_delete" "$prepared_list"
+	rm -f "$composition" "$unused_accounts" "$old_accounts" "$junk_to_delete" "$dbjunk_to_delete" "$prepared_accounts"
 }
 
-
-prepare_execution()
-{
-	if [ -s "$unused_accounts" ]; then
-		# prepare selected user list to be removed
-		sed -e 's/^/prosodyctl deluser /' "$unused_accounts" >> "$prepared_list"
-
-		if [ "$logging" = "true" ]; then
-			# read the files line by line and prepend and append some info
-			while read -r line; do
-				log_to_file "$(echo -e "$line" | sed -e 's/^/Registration expired: /')"
-			done < "$unused_accounts"
-		fi
-	fi
-
-	if [ -s "$old_accounts" ]; then
-		# prepare selected user list to be removed
-		sed -e 's/^/prosodyctl deluser /'  "$old_accounts" >> "$prepared_list"
-
-		if [ "$logging" = "true" ]; then
-			# read the files line by line and prepend and append some info
-			while read -r line; do
-				log_to_file "$(echo -e "$line" | sed -e 's/^/Account expired: /')"
-			done < "$old_accounts"
-		fi
-	fi
-
-	if [ -s "$junk_to_delete" ]; then
-		# prepare folder list to be removed
-		sed -e 's/^/rm -rf /' "$junk_to_delete" >> "$prepared_list"
-
-		if [ "$logging" = "true" ]; then
-			# read the files line by line and prepend and append some info
-			while read -r line; do
-				log_to_file "$(echo -e "$line" | sed -e 's/^/Folder: "/' | sed 's/$/" has been marked for removal./')"
-			done < "$junk_to_delete"
-		fi
-	fi
-}
-
-log_to_file()
-{
-	# ghetto logging
-	echo "[$(date --rfc-3339=seconds)] - $*" >> "$logfile"
-}
 
 ###### MAIN CODE SECTION ######
 # catch user help input
